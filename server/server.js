bb58c9b1e4e96eab2a2eceb6ff0ee1438a628dce
Pablo Moreno <pablonicolasmc5@gmail.com>
2025-04-13 18:20:14 -0400

    feat: connect backend to MongoDB Atlas using Mongoose

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); //middleware
+
+connectDB();
+
+app.listen(3000, () => console.log('Sever running on port 3000'));
