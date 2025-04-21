de9acefe2523a1ec93cb33d6f1187b4747051c17
pablomoreno10 <pablonicolasmc5@gmail.com>
2025-04-20 23:12:51 -0400

    fix: fix register controller, add role

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -14,7 +14,7 @@ const generateToken = (user) => {
 const registerUser = async (req, res) => {
 
     try {
-        const {name, email, password} = req.body;
+        const {name, email, password, role} = req.body;
         if(!allowedEmails.includes(email.toLowerCase())){
             return res.status(403).json({message: 'Email is not authorized'});
         }
@@ -24,7 +24,7 @@ const registerUser = async (req, res) => {
             return res.status(400).json({message: "User already exists"});
         }
 
-        const user = await User.create({name, email, password});
+        const user = await User.create({name, email, password, role});
 
         res.status(201).json(
             {
