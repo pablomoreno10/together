8ebc59f9e6c9cee96ebada0002321da4120ea009
pablomoreno10 <pablonicolasmc5@gmail.com>
2025-04-27 23:33:40 -0400

    feat: implement basic Socket io to backend for real-time messaging

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
-/*const http = require('http');
-const {Server} = require('socket.io');*/
+const http = require('http');
+const {Server} = require('socket.io');
 
 const express = require('express');
 const app = express();
-/*cconst server = http.createServer(app); //Wrapping express app into an HTTP server manually
-const io = new Server(server, {
-    cors: { origin: '*',}
-  });*/
+const server = http.createServer(app); //Wrapping express app into an HTTP server manually
+const io = new Server(server, {cors: { origin: '*',}}); //Will connect to frontend later on
 
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
@@ -18,16 +16,18 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/todos', require('./routes/toDos'));
 app.use('/api/events', require('./routes/events'));
 
-/*io.on('connection', (socket) => {
+io.on('connection', (socket) => {
     console.log('user connected', socket.id);
 
-    socket.on('send_message', (data) => {
-        io.to(data.teamId).emit('receive_message', data);
+    socket.join('global');
+    
+    socket.on('send_message', ({sender, text}) => {
+        io.to('global').emit('receive_message', {sender, text});
     });
     socket.on('disconnect', () => {
         console.log('user disconnected', socket.id);
         });
-  });*/
+});
 
 
-app.listen(3000, () => console.log('Sever running on port 3000'));
+server.listen(3000, () => console.log('Sever running on port 3000'));
