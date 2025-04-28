58033a481a049985a54ef3a1db4a3984dd228817
pablomoreno10 <pablonicolasmc5@gmail.com>
2025-04-27 20:24:37 -0400

    feat: add message model for team messaging feature

---
+++ server/models/chat.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    userId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    teamId: { 
+        type: String,
+        required: true,
+
+    },
+    text: { 
+      type: String, 
+      required: true 
+    },
+    createdAt: { 
+      type: Date, 
+      default: Date.now 
+    }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
+/*const http = require('http');
+const {Server} = require('socket.io');*/
+
 const express = require('express');
+const app = express();
+/*cconst server = http.createServer(app); //Wrapping express app into an HTTP server manually
+const io = new Server(server, {
+    cors: { origin: '*',}
+  });*/
+
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
 dotenv.config();
-
-const app = express();
-
 app.use(express.json()); 
-
 connectDB();
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/todos', require('./routes/toDos'));
 app.use('/api/events', require('./routes/events'));
+
+/*io.on('connection', (socket) => {
+    console.log('user connected', socket.id);
+
+    socket.on('send_message', (data) => {
+        io.to(data.teamId).emit('receive_message', data);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected', socket.id);
+        });
+  });*/
+
 
 app.listen(3000, () => console.log('Sever running on port 3000'));
