b93bffc46abe5cd0f7554e18df660d151341cba9
pablomoreno10 <pablonicolasmc5@gmail.com>
2025-06-25 23:57:48 -0400

    update socket controller to handle updates and conntect to chat model

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
-    userId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User', 
-        required: true 
-    },
-    teamId: { 
-        type: String,
-        required: true,
-
-    },
-    text: { 
-      type: String, 
-      required: true 
-    },
-    timestamp: { 
-      type: Date, 
-      default: Date.now },
-
+  sender: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+  teamId: { 
+    type: String,
+    required: true,
+  },
+  text: { 
+    type: String, 
+    required: true 
+  },
+  timestamp: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
+
 
 module.exports = mongoose.model('Message', messageSchema);
--- server/sockets/sockerController.js
+++ server/sockets/sockerController.js
@@ -1,4 +1,7 @@
 //const jwt = require('jsonwebtoken'); will add authentication later on
+const Message = require("../models/chat");
+
+
 
 function handlerSocket(io) {
     io.on('connection', (socket) => {
@@ -9,14 +12,25 @@ function handlerSocket(io) {
         console.log(`Socket ${socket.id} joined team room: ${teamId}`);
       });
   
-      socket.on('send_message', ({ teamId, sender, text }) => {
-        io.to(teamId).emit('receive_message', { sender, text });
-      });
-  
-      socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
+      socket.on('send_message', async ({ teamId, senderId, text }) => {
+        const message = await Message.create({
+          sender: senderId,
+          teamId,
+          text
+        });
+        const populatedMessage = await message.populate('sender', 'name');
+       io.to(teamId).emit('newMessage', {
+        _id: message._id,
+        text: message.text,
+        sender: populatedMessage.sender.name,
+        timestamp: message.timestamp,
       });
     });
-}
 
-module.exports = {handlerSocket};
+    socket.on('disconnect', () => {
+      console.log('User disconnected:', socket.id);
+    });
+  });
+};
+
+module.exports = { handlerSocket };
