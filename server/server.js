e01c4382fb941dbf98406021388a4f3c17e17297
pablomoreno10 <pablonicolasmc5@gmail.com>
2025-06-29 17:03:35 -0400

    env variable instead of hardcoded URL, best practice

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_ORIGIN,
   credentials: true
 }));
 const server = http.createServer(app); 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', 
+    origin: process.env.CLIENT_ORIGIN, 
     credentials: true
   }
 });
